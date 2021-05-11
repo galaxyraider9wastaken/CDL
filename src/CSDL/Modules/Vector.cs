4b431921ccca92581b55fb3f59859bb74bb87ff2
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-11 13:54:50 -0700

    Update and rename LinearAlgebra to Vector.cs

---
+++ src/CSDL/Modules/Vector.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace LinearAlgebra{
+  class Vector2{
+    public float X;
+    public float Y;
+    public Vector2(float x, Float y){
+      X = x;
+      Y = y;
+    }
+  }
+}
+namespace LinearAlgebra{
+  class Vector3{
+    public float X;
+    public float Y;
+    public Vector2(float x, Float y){
+      X = x;
+      Y = y;
+    }
+  }
+}
+namespace LinearAlgebra{
+  class Vector3{
+    public float X;
+    public float Y;
+    public float Z;
+    public Vector3(float x, Float y, float z, float w){
+      X = x;
+      Y = y;
+      Z = z;
+    }
+  }
+}
+namespace LinearAlgebra{
+  class Vector3{
+    public float X;
+    public float Y;
+    public float Z;
+    public float W;
+    public Vector3(float x, Float y, float z, float w){
+      X = x;
+      Y = y;
+      Z = z;
+      W = w;
+    }
+  }
+}
