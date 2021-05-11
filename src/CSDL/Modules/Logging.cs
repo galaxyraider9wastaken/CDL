0e7a96f90678fab44da51b5b31db2f6325904177
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-11 13:01:36 -0700

    Update Logging.cs

--- src/CSDL/Modules/Logging.cs
+++ src/CSDL/Modules/Logging.cs
@@ -1,1 +1,12 @@
+using System;
 
+namespace Logging{
+  public class Log{
+    private int LogState;
+    private int Path;
+    public Log(int logState, string path = ""){
+      LogState = logState;
+      Path = path
+    }
+  }
+}
