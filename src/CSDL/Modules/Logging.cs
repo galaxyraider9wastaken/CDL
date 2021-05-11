e44cfa42ce296db33e4f94716e5ab05b688fe85a
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-11 13:16:14 -0700

    Update Logging.cs

--- src/CSDL/Modules/Logging.cs
+++ src/CSDL/Modules/Logging.cs
@@ -8,5 +8,24 @@ namespace Logging{
       LogState = logState;
       Path = path
     }
+    public void LogMsg(string Msg, int Line, string FileName, int MessageStatus = 0){
+      if(LogState == 1){
+        
+      }
+      if(LogState == 2){
+        if(MessageStatus = 0){
+          Console.WriteLine("Log:: File - " + File + "Line - " + Line " >"  + "Status 0 - Message::" + Msg + "::EndLog");
+        }
+        if(MessageStatus = 1){
+          Console.WriteLine("Log:: File - " + File + "Line - " + Line " >" + "Status 1 - Warning::" + Msg + "::EndLog");
+        }
+        if(MessageStatus = 2){
+          Console.WriteLine("Log:: File - " + File + "Line - " + Line " >" + "Status 2 - Error::" + Msg + "::EndLog");
+        }
+      }
+    }
+    private void Write(string Msg, int MessageStatus){
+      
+    }
   }
 }
