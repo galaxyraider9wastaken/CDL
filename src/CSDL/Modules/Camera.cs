edd44294916315fafe318fc6108c452030cbac84
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-13 13:21:25 -0700

    Update Camera.cs

--- src/CSDL/Modules/Camera.cs
+++ src/CSDL/Modules/Camera.cs
@@ -8,6 +8,10 @@ namespace CSDL.Graphics{
     }
     public void FollowPosition(Vector3 Position, int WinWidth, int WinHeight){
       CameraPos = new Vector3(Position.X - WinWidth/2, Position.Y - WinHeight/2, Position.Z);
+      using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Camera.txt", true))  
+      {  
+        writer.WriteLine(CameraPos.X+","+CameraPos.Y+","CameraPos.Z);
+      }  
     }
   }
 }
