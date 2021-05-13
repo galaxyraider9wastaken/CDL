e10cadb95006a73dffedaf3e42c99b738ea42468
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-13 10:15:41 -0700

    Update Matrix.cs

--- src/CSDL/Modules/Matrix.cs
+++ src/CSDL/Modules/Matrix.cs
@@ -1,1 +1,15 @@
+using System;
 
+namespace CSDL.LinearAlgebra{
+  public class Matrix{
+    public float[][] Translation = new float[][];
+    public Vector2 Size;
+    public Matrix(Vector2 size){
+      Size = size;
+      Translation = new float[(int)Size.X][];
+      for(int i =0;i<Translation.Lenght;i++){
+        Translation[i] = new float[(int)Size.Y];
+      }
+    }
+  }
+}
