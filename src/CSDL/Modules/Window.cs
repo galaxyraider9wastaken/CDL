bb8c322efda9aaf95c1e5ee6a609b25f406bf696
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-13 12:46:32 -0700

    Update Window.cs

--- src/CSDL/Modules/Window.cs
+++ src/CSDL/Modules/Window.cs
@@ -8,22 +8,19 @@ namespace CSDL.Graphics{
     private int Y;
     private int Color;
     private string Title;
-    public Window(int width, int height, int x, int y, Color color, string title){s
+    public Window(int width, int height, int x, int y, Color color, string title){
       Width = width;
       Height = height;
       X = x;
       Y = y;
       Color = color;
       Title = title;
-      using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Windows.txt", true))  
+      using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Window.txt", true))  
       {  
-        writer.WriteLine("Draw");
+        writer.WriteLine(Width+","+Height+","+X+","+Y+","+Color+","+Title);
       }  
     }
-    public void UpdateWindow(){
-      
-    }
-    public void DrawWindow(){
+    public void Update(){
       
     }
   }
