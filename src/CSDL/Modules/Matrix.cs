3822a25d809c6a18c22f5f7f6eb757ca2331e8a8
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-13 10:25:43 -0700

    Update Matrix.cs

--- src/CSDL/Modules/Matrix.cs
+++ src/CSDL/Modules/Matrix.cs
@@ -13,3 +13,16 @@ namespace CSDL.LinearAlgebra{
     }
   }
 }
+namespace CSDL.LinearAlgebra{
+  public class MatrixEquations{
+    public static Matrix Add(int MSize, Matrix M1, Matrix M2){
+      var MA = new Matrix(new Vector2(MSize, MSize));
+      for(int i =0;i<MSize;i++){
+        for(int i1=0;i1<MSize;i1++){
+          MA.Transformation[i][i1] = M1.Transformation[i][i1] + M2.Transformation[i][i1];
+        }
+      }
+      return MA;
+    }
+  }
+}
