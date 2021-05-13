5fbcbbae0560ed873648b4b824dd8afb783f5bbc
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-13 12:59:53 -0700

    Create Drawing.cs

---
+++ src/CSDL/Modules/Drawing.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CSDL.Graphics{
+  public class DrawBatch{
+    public static void Draw2D(Vector2 Position, Vector2 Size, Vector2 Rotation, Texture2D, int RFilter){
+      
+    }
+  }
+}
