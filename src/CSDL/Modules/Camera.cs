b0cb00348ad0f49f52e7c33d6876459b5276e8da
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-14 12:14:26 -0700

    Update Camera.cs

--- src/CSDL/Modules/Camera.cs
+++ src/CSDL/Modules/Camera.cs
@@ -3,14 +3,19 @@ using System;
 namespace CSDL.Graphics{
   public class Camera{
     private Vector3 CameraPos;
-    public Camera(){
-    
+    private Vector2 ViewPort;
+    public Camera(Vector2 viewPort){
+      ViewPort = viewPort;
+      using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Camera.txt", false))  
+      {  
+        writer.WriteLine(CameraPos.X+","+CameraPos.Y+","CameraPos.Z+","+ViewPort.X+","+ViewPort.Y);
+      }  
     }
     public void FollowPosition(Vector3 Position, int WinWidth, int WinHeight){
       CameraPos = new Vector3(Position.X - WinWidth/2, Position.Y - WinHeight/2, Position.Z);
-      using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Camera.txt", true))  
+      using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Camera.txt", false))  
       {  
-        writer.WriteLine(CameraPos.X+","+CameraPos.Y+","CameraPos.Z);
+        writer.WriteLine(CameraPos.X+","+CameraPos.Y+","CameraPos.Z+","+ViewPort.X+","+ViewPort.Y);
       }  
     }
   }
