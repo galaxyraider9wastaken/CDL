5475e32f40dc2357adfede8cd7b8b0de41651488
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-14 10:12:53 -0700

    Update Vector.h

--- src/CPPDL/Modules/Vector.h
+++ src/CPPDL/Modules/Vector.h
@@ -7,20 +7,20 @@
 
 class Vector{
 public:
-  Vector2 AddVec2(Vector2 V1, Vector2 V2);
-  Vector3 AddVec3(Vector3 V1, Vector3 V2);
-  Vector4 AddVec4(Vector4 V1, Vector4 V2);
-  Vector2 SubtractVec2(Vector2 V1, Vector2 V2);
-  Vector3 SubtractVec3(Vector3 V1, Vector3 V2);
-  Vector4 SubtractVec4(Vector4 V1, Vector4 V2);
-  Vector2 MultiplyVec2(Vector2 V1, Vector2 V2);
-  Vector3 MultiplyVec3(Vector3 V1, Vector3 V2);
-  Vector4 MultiplyVec4(Vector4 V1, Vector4 V2);
-  Vector2 DivideVec2(Vector2 V1, Vector2 V2);
-  Vector3 DivideVec3(Vector3 V1, Vector3 V2);
-  Vector4 DivideVec4(Vector4 V1, Vector4 V2);
-  Vector2 NormalizeVec2(Vector2 V1, Vector2 V2);
-  Vector3 NormalizeVec3(Vector3 V1, Vector3 V2);
-  Vector4 NormalizeVec4(Vector4 V1, Vector4 V2);
+  static Vector2 AddVec2(Vector2 V1, Vector2 V2);
+  static Vector3 AddVec3(Vector3 V1, Vector3 V2);
+  static Vector4 AddVec4(Vector4 V1, Vector4 V2);
+  static Vector2 SubtractVec2(Vector2 V1, Vector2 V2);
+  static Vector3 SubtractVec3(Vector3 V1, Vector3 V2);
+  static Vector4 SubtractVec4(Vector4 V1, Vector4 V2);
+  static Vector2 MultiplyVec2(Vector2 V1, Vector2 V2);
+  static Vector3 MultiplyVec3(Vector3 V1, Vector3 V2);
+  static Vector4 MultiplyVec4(Vector4 V1, Vector4 V2);
+  static Vector2 DivideVec2(Vector2 V1, Vector2 V2);
+  static Vector3 DivideVec3(Vector3 V1, Vector3 V2);
+  static Vector4 DivideVec4(Vector4 V1, Vector4 V2);
+  static Vector2 NormalizeVec2(Vector2 V1, Vector2 V2);
+  static Vector3 NormalizeVec3(Vector3 V1, Vector3 V2);
+  static Vector4 NormalizeVec4(Vector4 V1, Vector4 V2);
 };
 #endif
