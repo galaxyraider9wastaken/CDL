39f6244b46e9461f8f8b006ba134beb7a4e4e29b
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-17 09:25:50 -0700

    Update Camera.cs

--- src/CSDL/Modules/Camera.cs
+++ src/CSDL/Modules/Camera.cs
@@ -4,18 +4,20 @@ namespace CSDL.Graphics{
   public class Camera{
     private Vector3 CameraPos;
     private Vector2 ViewPort;
-    public Camera(Vector2 viewPort){
+    public Window Window;
+    public Camera(Vector2 viewPort, Window window){
       ViewPort = viewPort;
+      Window = window;
       using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Camera.txt", false))  
       {  
-        writer.WriteLine(CameraPos.X+","+CameraPos.Y+","CameraPos.Z+","+ViewPort.X+","+ViewPort.Y+";");
+        writer.WriteLine(CameraPos.X+","+CameraPos.Y+","CameraPos.Z+","+ViewPort.X+","+ViewPort.Y+","+Window.DWD_ID+";");
       }  
     }
     public void FollowPosition(Vector3 Position, int WinWidth, int WinHeight){
       CameraPos = new Vector3(Position.X - WinWidth/2, Position.Y - WinHeight/2, Position.Z);
       using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Camera.txt", false))  
       {  
-        writer.WriteLine(CameraPos.X+","+CameraPos.Y+","CameraPos.Z+","+ViewPort.X+","+ViewPort.Y+";");
+        writer.WriteLine(CameraPos.X+","+CameraPos.Y+","CameraPos.Z+","+ViewPort.X+","+ViewPort.Y+","+Window.DWD_ID+";");
       }  
     }
   }
