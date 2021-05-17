9d6503d1a3e220ea38373ea5394b0a4efbd376a6
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-17 09:05:03 -0700

    Update Window.cs

--- src/CSDL/Modules/Window.cs
+++ src/CSDL/Modules/Window.cs
@@ -15,6 +15,7 @@ namespace CSDL.Graphics{
       Title = title;
       FullScreen = fullScreen;
       WindowNum+=1;
+      DWD_ID = DrawingWindowID + WindowNum;
       using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Window.txt", true))  
       {  
         writer.WriteLine(Width+","+Height+","+X+","+Y+","+Color.RGBA.X+","+Color.RGBA.Y+","+Color.RGBA.Z+","+Color.RGBA.W+","+Title+","+FullScreen+","+DrawingWindowID+","+DWD_ID";");
