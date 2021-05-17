a77b8e1b0cb4dffa3c2b2e18ff777dd58fd84657
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-17 09:03:43 -0700

    Update Globals.cs

--- src/CSDL/Modules/Globals.cs
+++ src/CSDL/Modules/Globals.cs
@@ -2,5 +2,6 @@ namespace CSDL{
   public class Globals(){
       public static string DrawingBatchDataPath;
       public static int DrawingWindowID = 101;
+      public static int WindowNum;
   }
 }
