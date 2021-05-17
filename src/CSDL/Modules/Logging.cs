7584200e7572bc1ad80bc7478fe8f3dabb80853a
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-17 09:53:50 -0700

    Update Logging.cs

--- src/CSDL/Modules/Logging.cs
+++ src/CSDL/Modules/Logging.cs
@@ -1,43 +1,44 @@
 using System;
+using System.IO;
 
 namespace CSDL.Logging{
   public class Log{
     private int LogState;
-    private int Path;
+    private string Path;
     public Log(int logState, string path = ""){
       LogState = logState;
-      Path = path
+      Path = path;
     }
     public void LogMsg(string Msg, int Line, string FileName, int MessageStatus = 0){
       if(LogState == 1){
-        if(MessageStatus = 0){
-          using (StreamWriter writer = new StreamWriter(Path, true))  
+        if(MessageStatus == 0){
+          using (StreamWriter writer = new StreamWriter(Path, true))
           {  
-            writer.WriteLine("Log:: File - " + File + "Line - " + Line " >"  + "Status 0 - Message::" + Msg + "::EndLog");
+            writer.WriteLine("Log:: File - " + FileName + "Line - " + Line + " >"  + "Status 0 - Message::" + Msg + "::EndLog");
           }  
         }
-        if(MessageStatus = 1){
+        if(MessageStatus == 1){
            using (StreamWriter writer = new StreamWriter(Path, true))  
           {  
-            writer.WriteLine("Log:: File - " + File + "Line - " + Line " >"  + "Status 1 - Warning::" + Msg + "::EndLog");
+            writer.WriteLine("Log:: File - " + FileName + "Line - " + Line + " >"  + "Status 1 - Warning::" + Msg + "::EndLog");
           }  
         }
-        if(MessageStatus = 2){
+        if(MessageStatus == 2){
           using (StreamWriter writer = new StreamWriter(Path, true))  
           {  
-            writer.WriteLine("Log:: File - " + File + "Line - " + Line " >"  + "Status 2 - Error::" + Msg + "::EndLog");
+            writer.WriteLine("Log:: File - " + FileName + "Line - " + Line + " >"  + "Status 2 - Error::" + Msg + "::EndLog");
           }
         }
       }
       if(LogState == 2){
-        if(MessageStatus = 0){
-          Console.WriteLine("Log:: File - " + File + "Line - " + Line " >"  + "Status 0 - Message::" + Msg + "::EndLog");
+        if(MessageStatus == 0){
+          Console.WriteLine("Log:: File - " + FileName + "Line - " + Line + " >"  + "Status 0 - Message::" + Msg + "::EndLog");
         }
-        if(MessageStatus = 1){
-          Console.WriteLine("Log:: File - " + File + "Line - " + Line " >" + "Status 1 - Warning::" + Msg + "::EndLog");
+        if(MessageStatus == 1){
+          Console.WriteLine("Log:: File - " + FileName + "Line - " + Line + " >" + "Status 1 - Warning::" + Msg + "::EndLog");
         }
-        if(MessageStatus = 2){
-          Console.WriteLine("Log:: File - " + File + "Line - " + Line " >" + "Status 2 - Error::" + Msg + "::EndLog");
+        if(MessageStatus == 2){
+          Console.WriteLine("Log:: File - " + FileName + "Line - " + Line + " >" + "Status 2 - Error::" + Msg + "::EndLog");
         }
       }
     }
