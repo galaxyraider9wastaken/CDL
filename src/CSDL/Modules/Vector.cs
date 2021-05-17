412092854c0782ad03ead3ff2534ccbbd59b546b
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-17 09:17:27 -0700

    Update Vector.cs

--- src/CSDL/Modules/Vector.cs
+++ src/CSDL/Modules/Vector.cs
@@ -4,17 +4,7 @@ namespace CSDL.LinearAlgebra{
   public class Vector2{
     public float X;
     public float Y;
-    public Vector2(float x, Float y){
-      X = x;
-      Y = y;
-    }
-  }
-}
-namespace CSDL.LinearAlgebra{
-  public class Vector3{
-    public float X;
-    public float Y;
-    public Vector2(float x, Float y){
+    public Vector2(float x, float y){
       X = x;
       Y = y;
     }
@@ -25,7 +15,7 @@ namespace CSDL.LinearAlgebra{
     public float X;
     public float Y;
     public float Z;
-    public Vector3(float x, Float y, float z, float w){
+    public Vector3(float x, float y, float z, float w){
       X = x;
       Y = y;
       Z = z;
@@ -33,12 +23,12 @@ namespace CSDL.LinearAlgebra{
   }
 }
 namespace CSDL.LinearAlgebra{
-  public class Vector3{
+  public class Vector4{
     public float X;
     public float Y;
     public float Z;
     public float W;
-    public Vector3(float x, Float y, float z, float w){
+    public Vector4(float x, float y, float z, float w){
       X = x;
       Y = y;
       Z = z;
@@ -48,7 +38,7 @@ namespace CSDL.LinearAlgebra{
 }
 namespace CSDL.LinearAlgebra{
   public class Vector {
-    public Vector4 Add(int VType, float X1, float Y1, float Z1 = 0, float W1 = 0, float X2, float Y2, float Z2 = 0, float W2 = 0)){
+    public Vector4 Add(int VType, float X1, float Y1, float Z1, float W1, float X2, float Y2, float Z2, float W2){
       var VA = new Vector4(0, 0, 0, 0);
       if(VType == 2){
         VA.X = X1 + X2;
@@ -67,7 +57,7 @@ namespace CSDL.LinearAlgebra{
       }
       return VA;
     }
-    public Vector4 Subtract(int VType, float X1, float Y1, float Z1 = 0, float W1 = 0, float X2, float Y2, float Z2 = 0, float W2 = 0)){
+    public Vector4 Subtract(int VType, float X1, float Y1, float Z1, float W1, float X2, float Y2, float Z2, float W2){
       var VA = new Vector4(0, 0, 0, 0);
       if(VType == 2){
         VA.X = X1 - X2;
@@ -86,7 +76,7 @@ namespace CSDL.LinearAlgebra{
       }
       return VA;
     }
-    public Vector4 Multiply(int VType, float X1, float Y1, float Z1 = 0, float W1 = 0, float X2, float Y2, float Z2 = 0, float W2 = 0)){
+    public Vector4 Multiply(int VType, float X1, float Y1, float Z1, float W1, float X2, float Y2, float Z2, float W2){
       var VA = new Vector4(0, 0, 0, 0);
       if(VType == 2){
         VA.X = X1 * X2;
@@ -105,7 +95,7 @@ namespace CSDL.LinearAlgebra{
       }
       return VA;
     }
-    public Vector4 Divide(int VType, float X1, float Y1, float Z1 = 0, float W1 = 0, float X2, float Y2, float Z2 = 0, float W2 = 0)){
+    public Vector4 Divide(int VType, float X1, float Y1, float Z1, float W1, float X2, float Y2, float Z2, float W2){
       var VA = new Vector4(0, 0, 0, 0);
       if(VType == 2){
         VA.X = X1 / X2;
@@ -124,20 +114,20 @@ namespace CSDL.LinearAlgebra{
       }
       return VA;
     }
-    public Vector4 Normalize(int VType, float X1, float Y1, float Z1 = 0, float W1 = 0, float X2, float Y2, float Z2 = 0, float W2 = 0){
-      var distance = 0f;
-      var VA = new Vector4();
+    public Vector4 Normalize(int VType, float X1, float Y1, float Z1, float W1, float X2, float Y2, float Z2, float W2){
+      double distance;
+      var VA = new Vector4(0, 0, 0, 0);
       if(VType == 2){
         distance = Math.Sqrt(X1 * X2 + Y1 * Y2);
-        VA = new Vector4(X1/distance, Y1/distance, 0, 0);
+        VA = new Vector4(X1/(float)distance, Y1/(float)distance, 0, 0);
       }
       if(VType == 3){
         distance = Math.Sqrt(X1 * X2 + Y1 * Y2 + Z1 * Z2);
-        VA = new Vector4(X1/distance, Y1/distance, Z/distance, 0);
+        VA = new Vector4(X1/(float)distance, Y1/(float)distance, Z1/(float)distance, 0);
       }
       if(VType == 4){
         distance = Math.Sqrt(X1 * X2 + Y1 * Y2 + Z1 * Z2 + W1 * W2);
-        VA = new Vector4(X1/distance, Y1/distance, Z/distance, W / distance);
+        VA = new Vector4(X1/(float)distance, Y1/(float)distance, Z1/(float)distance, W1/(float)distance);
       }
       return VA;
     }
