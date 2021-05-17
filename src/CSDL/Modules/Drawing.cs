e9d69a119049b05b4b99ae40acf51a575657d2e0
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-17 09:01:55 -0700

    Update Drawing.cs

--- src/CSDL/Modules/Drawing.cs
+++ src/CSDL/Modules/Drawing.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace CSDL.Graphics{
   public class DrawBatch : Globals{
-    public static void Draw2D(Vector2 Position, Vector2 Size, Vector2 Rotation, Texture2D Texture, int RFilter){
+    public static void Draw2D(Window window, Vector2 Position, Vector2 Size, Vector2 Rotation, Texture2D Texture, int RFilter){
       using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Sprite2D.txt", true))  
       {  
         writer.WriteLine(Position.X+","+Position.Y+","+Size.X+","+Size.Y+","+Rotation.X+","+Rotation.Y+","+Texture.Path+","+RFilter+","+DrawingWindowID+";");
@@ -11,7 +11,7 @@ namespace CSDL.Graphics{
     public static void ClearBatch2D(){
       System.IO.File.WriteAllText(DrawingBatchDataPath + "Sprite2D.txt",string.Empty);
     }
-    public static void Draw3D(Vector3 Position, Vector3 Size, Vector3 Rotation, Texture3D Texture){
+    public static void Draw3D(Window window, Vector3 Position, Vector3 Size, Vector3 Rotation, Texture3D Texture){
       using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Sprite3D.txt", true))  
       {  
         writer.WriteLine(Position.X+","+Position.Y+","+Position.Z+","+Size.X+","+Size.Y+","+Size.Z+","+Rotation.X+","+Rotation.Y+","+Rotation.Z+","+Texture.Path+","+DrawingWindowID+";");
