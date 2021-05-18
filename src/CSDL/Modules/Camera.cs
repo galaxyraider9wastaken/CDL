c500f0ca69f012ff83eeb584de881c87c5e5d4bc
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-18 10:25:10 -0700

    Update Camera.cs

--- src/CSDL/Modules/Camera.cs
+++ src/CSDL/Modules/Camera.cs
@@ -5,20 +5,24 @@ namespace CSDL.Graphics{
     private Vector3 CameraPos;
     private Vector2 ViewPort;
     public Window Window;
+    public Vector3 Orientation;
     public Camera(Vector2 viewPort, Window window){
       ViewPort = viewPort;
       Window = window;
       using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Camera.txt", false))  
       {  
-        writer.WriteLine(CameraPos.X+","+CameraPos.Y+","CameraPos.Z+","+ViewPort.X+","+ViewPort.Y+","+Window.DWD_ID+";");
+        writer.WriteLine(CameraPos.X+","+CameraPos.Y+","CameraPos.Z+","+ViewPort.X+","+ViewPort.Y+","+Orientation.X+","+Orientation.Y+","+Orientation.Z+","+Window.DWD_ID+";");
       }  
     }
-    public void FollowPosition(Vector3 Position, int WinWidth, int WinHeight){
+    public void FollowPosition2D(Vector2 Position, int WinWidth, int WinHeight){
       CameraPos = new Vector3(Position.X - WinWidth/2, Position.Y - WinHeight/2, Position.Z);
       using (StreamWriter writer = new StreamWriter(DrawingBatchDataPath + "Camera.txt", false))  
       {  
-        writer.WriteLine(CameraPos.X+","+CameraPos.Y+","CameraPos.Z+","+ViewPort.X+","+ViewPort.Y+","+Window.DWD_ID+";");
+        writer.WriteLine(CameraPos.X+","+CameraPos.Y+ViewPort.X+","+ViewPort.Y+","+Window.DWD_ID+";");
       }  
+    }
+    public void SetOrientation(Vector3 orientation){
+      Orientation = orientation;
     }
   }
 }
