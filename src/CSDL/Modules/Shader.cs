1d2b57be505e217f0bf30c18af14782536c3fc71
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-18 12:55:57 -0700

    Update Shader.cs

--- src/CSDL/Modules/Shader.cs
+++ src/CSDL/Modules/Shader.cs
@@ -7,10 +7,32 @@ namespace CSDL.Graphics{
     public Shader(int shaderType, string shaderPath){
       ShaderType = shaderType;
       ShaderPath = shaderPath;
-      using (StreamWriter writer = new StreamWriter(ShaderDataPath + "Shader.txt", true))
+    }
+  }
+}
+namespace CSDL.Graphics{
+  public class ShaderManager : Globals{
+    public static void SetVertexShader(Shader Shader){
+      using (StreamWriter writer = new StreamWriter(ShaderDataPath + "VertexShaderPath.txt", true))
       {  
-        writer.WriteLine(ShaderType+","+ShaderPath+";");
-      }  
+        writer.WriteLine(ShaderPath+";");
+      }
+    }
+    public static void SetFragmentShader(Shader Shader){
+      using (StreamWriter writer = new StreamWriter(ShaderDataPath + "FragmentShaderPath.txt", true))
+      {  
+        writer.WriteLine(ShaderPath+";");
+      }
+    }
+    public static void UseDeafaultShaders(){
+      using (StreamWriter writer = new StreamWriter(ShaderDataPath + "VertexShaderPath.txt", true))
+      {  
+        writer.WriteLine("Deafult.vert"+";");
+      }
+      using (StreamWriter writer = new StreamWriter(ShaderDataPath + "FragmentShaderPath.txt", true))
+      {  
+        writer.WriteLine("Deafult.frag"+";");
+      }
     }
   }
 }
