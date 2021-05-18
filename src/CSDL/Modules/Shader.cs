bb52b7239cbf9ae488937ec4e9479512eb00d92c
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-18 12:18:26 -0700

    Update Shader.cs

--- src/CSDL/Modules/Shader.cs
+++ src/CSDL/Modules/Shader.cs
@@ -1,9 +1,16 @@
 using System;
 
 namespace CSDL.Graphics{
-  public class Shader{
+  public class Shader : Globals{
+    public int ShaderType;
+    public string ShaderPath;
     public Shader(int shaderType, string shaderPath){
-      
+      ShaderType = shaderType;
+      ShaderPath = shaderPath;
+      using (StreamWriter writer = new StreamWriter(ShaderDataPath + "Shader.txt", true))
+      {  
+        writer.WriteLine(ShaderType+","+ShaderPath+";");
+      }  
     }
   }
 }
