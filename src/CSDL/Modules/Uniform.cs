90748b44608adc8161d019bed3f4af8ca5ae0c58
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-18 12:31:37 -0700

    Update Uniform.cs

--- src/CSDL/Modules/Uniform.cs
+++ src/CSDL/Modules/Uniform.cs
@@ -2,11 +2,20 @@ using System;
 
 namespace CDL.Graphics{
   public class Uniform(){
+    public string Name;
+    public Shader Shader;
     public Uniform(){
       
     }
-    public void GetVaule(){
-      
+    public void CreateUniform(int DataType, string DataFilePath){
+      using (StreamWriter writer = new StreamWriter(Path, true))
+      {  
+        writer.WriteLine(Name+","+Shader.ShaderPath+","+Shader.ShaderType+","+DataFilePath+","DataType+";");
+      }  
+    }
+    public void GetLocation(Shader shader, string name){
+      Name = name;
+      Shader = shader;
     }
   }
 }
