945bcfe55e53780f16d98c615ebe360d0cefd1d8
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-20 09:46:09 -0700

    Update Scene.cs

--- src/CSDL/Modules/Scene.cs
+++ src/CSDL/Modules/Scene.cs
@@ -10,11 +10,25 @@ namespace CSDL.Graphics{
       FragmentShader = fragmentShader;
       Windows = windows;
     }
+    public void SetSceneVertexShader(Shader shader){
+      VertexShader = shader;
+      using (StreamWriter writer = new StreamWriter(ScenePath + "VertexShaderPath.txt", true))
+      {  
+        writer.WriteLine(shader.ShaderPath+";");
+      }
+    }
+    public void SetSceneFragmentShader(Shader shader){
+      FragmentShader = shader;
+      using (StreamWriter writer = new StreamWriter(ScenePath + "FragmentShaderPath.txt", true))
+      {  
+        writer.WriteLine(shader.ShaderPath+";");
+      }
+    }
   }
 }
 
 namespace CSDL.Graphics{
-  public class Scene3D(){
+  public class Scene3D(Shader vertexShader, Shader fragmentShader, Window[] windows){
   
   }
 }
